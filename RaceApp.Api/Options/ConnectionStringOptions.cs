3b44029211f1d58e2f2e9b760daa599280af2820
Sandro Bormolini <sandro.bormolini@outlook.com>
2021-12-22 19:52:12 +0100

    Added options classes

---
+++ RaceApp.Api/Options/ConnectionStringOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace RaceApp.Api.Options;
+
+public class ConnectionStringOptions
+{
+    public Uri ServiceEndpoint { get; set; }
+    public string AuthKey { get; set; }
+
+    public void Deconstruct(out Uri serviceEndpoint, out string authKey)
+    {
+        serviceEndpoint = ServiceEndpoint;
+        authKey = AuthKey;
+    }
+}
---
+++ RaceApp.Api/Options/KeyVaultOptions.cs
@@ -0,0 +1,16 @@
+﻿namespace RaceApp.Api.Options;
+
+public enum KeyVaultUsage
+{
+    UseLocalSecretStore,
+    UseClientSecret,
+    UseMsi
+}
+
+public class KeyVaultOptions
+{
+    public KeyVaultUsage Mode { get; set; }
+    public string KeyVaultUri { get; set; }
+    public string ClientId { get; set; }
+    public string ClientSecret { get; set; }
+}
